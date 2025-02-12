01e9c4a8c8ba53a41a8209f9a4cf036a494a0134
RafifFarrelsyahFawwazka <rafifcer123@gmail.com>
2025-02-12 21:56:24 +0700

    Add files via upload

--- perulangan23.java
+++ perulangan23.java
@@ -1,29 +1,33 @@
 import java.util.Scanner;
+
 public class perulangan23 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        
+        System.out.print("Masukkan NIM Anda: ");
+        String nim = sc.nextLine();
+        
         int n;
-
-        System.out.print("Masukkan N (2 digit terakhir NIM anda) = ");
-        n = sc.nextInt();
-        if (n<=10) {
-            n+=10;
+        if (nim.length() >= 2) {
+            n = Integer.parseInt(nim.substring(nim.length() - 2));
+        } else {
+            System.out.println("NIM tidak valid!");
+            return;
         }
         
-
-        for (int i = 0;i<=n;i++) {
-
-            if (i % 2 != 0){
+        if (n <= 10) {
+            n += 10;
+        }
+        
+        System.out.println("n : " + n);
+        for (int i = 0; i <= n; i++) {
+            if (i % 2 != 0) {
                 System.out.print(" * ");
-            } else if  ( i==6 || i == 10) {
+            } else if (i == 6 || i == 10) {
                 continue;
             } else {
-                System.out.print(i);
+                System.out.print(i + " ");
             }
-            
         }
-
-
     }
 }
